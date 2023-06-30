2709207a1f08aaeaec17287eb17b3503f0f1bd3a
rocksana26 <rocksana2606@gmail.com>
2023-06-30 16:36:15 +0700

    second commit

--- game.js
+++ game.js
@@ -31,60 +31,69 @@ function drawFood() {
   ctx.fillRect(food.x, food.y, box, box);
 }
 //змея
-let snake = [{ x: box, y: box }];
-// let snake = {
-//   x: 5 * box,
-//   y: 5 * box,
-// };
+let snake = [
+  { x: 8 * box, y: 5 * box },
+  { x: 7 * box, y: 5 * box },
+  { x: 6 * box, y: 5 * box },
+  { x: 5 * box, y: 5 * box },
+];
 function drawSnake() {
   ctx.fillStyle = "#fff";
-  ctx.fillRect(snake[0].x, snake[0].y, box, box);
+  console.log(snake.length);
+  for (let i = 0; snake.length > i; i++) {
+    ctx.fillRect(snake[i].x, snake[i].y, box, box);
+  }
 }
+
 //клавиши
 window.addEventListener("keydown", keyDown);
-let xDirection = 0;
-let yDirection = 0;
+let dx = 0;
+let dy = 0;
+
+//движение
+function moveSnake() {
+  dx = box;
+  dy = 0;
+  const snakeHead = { x: snake[0].x + dx, y: snake[0].y + dy };
+  snake.unshift(snakeHead);
+  snake.pop();
+  //если вышла за стену
+  if (snake[0].x < 0) {
+    snake[0].x = canvas.clientWidth - box;
+  } else if (snake[0].x >= canvas.clientWidth) {
+    snake[0].x = 0;
+  } else if (snake[0].y < 0) {
+    snake[0].y = canvas.clientHeight - box;
+  } else if (snake[0].y >= canvas.clientHeight) {
+    snake[0].y = 0;
+  }
+}
 
 function keyDown(event) {
   //влево
-  if (event.keyCode === 37 && xDirection !== box) {
-    xDirection = -box;
-    yDirection = 0;
+  if (event.keyCode === 37 && dx !== box) {
+    dx = -box;
+    dy = 0;
   }
   //вверх
-  if (event.keyCode === 38 && yDirection !== box) {
-    xDirection = 0;
-    yDirection = -box;
+  if (event.keyCode === 38 && dy !== box) {
+    dx = 0;
+    dy = -box;
   }
   //вправо
-  if (event.keyCode === 39 && xDirection !== -box) {
-    xDirection = box;
-    yDirection = 0;
+  if (event.keyCode === 39 && dx !== -box) {
+    dx = box;
+    dy = 0;
   }
   //вниз
-  if (event.keyCode === 40 && yDirection !== -box) {
-    xDirection = 0;
-    yDirection = box;
-  }
-}
-//движение
-function changeSnakePos() {
-  snake.x = snake.x + xDirection;
-  snake.y = snake.y + yDirection;
-  //если вышла за стену
-  if (snake.x < 0) {
-    snake.x = canvas.clientWidth - box;
-  } else if (snake.x >= canvas.clientWidth) {
-    snake.x = 0;
-  } else if (snake.y < 0) {
-    snake.y = canvas.clientHeight - box;
-  } else if (snake.y >= canvas.clientHeight) {
-    snake.y = 0;
+  if (event.keyCode === 40 && dy !== -box) {
+    dx = 0;
+    dy = box;
   }
 }
 //змея съела яблоко
 function checkCollision() {
-  if (snake.x === food.x && snake.y === food.y) {
+  if (snake[0].x === food.x && snake[0].y === food.y) {
     food.x = (Math.floor(Math.random() * 16 - 1) + 1) * box;
     food.y = (Math.floor(Math.random() * 16 - 1) + 1) * box;
   }
@@ -94,9 +103,8 @@ function checkCollision() {
 function game() {
   drawChess();
   drawSnake();
-  changeSnakePos();
+  moveSnake();
   drawFood();
-
   checkCollision();
 }
 //запуск игры
