70909807f0849cc74c1d881564fc1263c0c5e87a
Diai <rocksana2606@gmail.com>
2023-07-03 01:37:41 +0700

    refactoring-code

--- game.js
+++ game.js
@@ -1,7 +1,5 @@
 import './style.scss';
 import 'reset-css';
-export let dx = 0;
-export let dy = 0;
 import { KeyCodes } from './helpers/keys';
 import {
   CANVAS,
@@ -11,6 +9,22 @@ import {
   ROWS_COUNT,
   Colors,
 } from './helpers/const';
+let score = 0;
+document.getElementById('score').innerHTML = `Счет: ${score}`;
+let dx = BOX_SIZE; // начальная скорость змеи
+let dy = 0;
+setupGame(); //запуск игры
+
+function setupGame() {
+  setTimeout(function onTick() {
+    drawChess();
+    drawFood();
+    moveSnake();
+    drawSnake();
+
+    setupGame();
+  }, 1000 / 7);
+}
 
 // фон
 function drawChess() {
@@ -35,27 +49,34 @@ function drawFood() {
 }
 
 //змея
-let snake = [
-  { x: 8 * BOX_SIZE, y: 5 * BOX_SIZE },
-  { x: 7 * BOX_SIZE, y: 5 * BOX_SIZE },
-  { x: 6 * BOX_SIZE, y: 5 * BOX_SIZE },
+let initalSnake = [
   { x: 5 * BOX_SIZE, y: 5 * BOX_SIZE },
+  { x: 4 * BOX_SIZE, y: 5 * BOX_SIZE },
+  { x: 3 * BOX_SIZE, y: 5 * BOX_SIZE },
+  { x: 2 * BOX_SIZE, y: 5 * BOX_SIZE },
 ];
+
+let snake = [...initalSnake];
+
 function drawSnake() {
   CTX.fillStyle = Colors.SnakeColor;
-  for (let i = 0; snake.length > i; i++) {
-    CTX.fillRect(snake[i].x, snake[i].y, BOX_SIZE, BOX_SIZE);
-  }
+  snake.forEach((part) => {
+    CTX.fillRect(part.x, part.y, BOX_SIZE, BOX_SIZE);
+  });
 }
 
-//клавиши
-window.addEventListener('keydown', keyDown);
-
-//движение
 function moveSnake() {
   const snakeHead = { x: snake[0].x + dx, y: snake[0].y + dy };
   snake.unshift(snakeHead);
-  snake.pop();
+  const hasEatFood = snake[0].x === food.x && snake[0].y === food.y;
+  if (hasEatFood) {
+    score += 1;
+    // счет
+    document.getElementById('score').innerHTML = `Счет: ${score}`;
+    generateFood();
+  } else {
+    snake.pop();
+  }
   //если вышла за стену
   if (snake[0].x < 0) {
     snake[0].x = CANVAS.clientWidth - BOX_SIZE;
@@ -68,47 +89,42 @@ function moveSnake() {
   }
 }
 
+function generateFood() {
+  food.x = (Math.floor(Math.random() * 16 - 1) + 1) * BOX_SIZE;
+  food.y = (Math.floor(Math.random() * 16 - 1) + 1) * BOX_SIZE;
+  //проверка если еда сгенерилась внутри змеи
+  snake.forEach((part) => {
+    const hasEat = part.x === food.x && part.y === food.y;
+    if (hasEat) generateFood();
+  });
+}
+
+//клавиши
+window.addEventListener('keydown', keyDown);
+
 function keyDown(event) {
-  const ArrowLeft = dx === BOX_SIZE;
-  const ArrowUp = dy === BOX_SIZE;
-  const ArrowRight = dx === -BOX_SIZE;
-  const ArrowDown = dy === -BOX_SIZE;
+  const isArrowLeftPressed = dx === BOX_SIZE;
+  const isArrowUpPressed = dy === BOX_SIZE;
+  const isArrowRightPressed = dx === -BOX_SIZE;
+  const isArrowDownPressed = dy === -BOX_SIZE;
   //влево
-  if (event.keyCode === KeyCodes.LEFT && !ArrowLeft) {
+  if (event.keyCode === KeyCodes.LEFT && !isArrowLeftPressed) {
     dx = -BOX_SIZE;
     dy = 0;
   }
   //вверх
-  if (event.keyCode === KeyCodes.UP && !ArrowUp) {
+  if (event.keyCode === KeyCodes.UP && !isArrowUpPressed) {
     dx = 0;
     dy = -BOX_SIZE;
   }
   //вправо
-  if (event.keyCode === KeyCodes.RIGHT && !ArrowRight) {
+  if (event.keyCode === KeyCodes.RIGHT && !isArrowRightPressed) {
     dx = BOX_SIZE;
     dy = 0;
   }
   //вниз
-  if (event.keyCode === KeyCodes.DOWN && !ArrowDown) {
+  if (event.keyCode === KeyCodes.DOWN && !isArrowDownPressed) {
     dx = 0;
     dy = BOX_SIZE;
   }
 }
-drawFood();
-//змея съела яблоко
-function checkCollision() {
-  if (snake[0].x === food.x && snake[0].y === food.y) {
-    food.x = (Math.floor(Math.random() * 16 - 1) + 1) * BOX_SIZE;
-    food.y = (Math.floor(Math.random() * 16 - 1) + 1) * BOX_SIZE;
-    setTimeout(drawFood(), 1000);
-  }
-}
-
-function game() {
-  drawChess();
-  drawSnake();
-  moveSnake();
-  checkCollision();
-}
-//запуск игры
-setInterval(game, 200);
